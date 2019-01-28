5d533fd737b33781b42e144b50a43956605ae54f
Anatolii <>
2019-01-28 13:08:35 +0200

    reverted original license

--- app/code/community/Lanot/Core/Block/Adminhtml/System/Config/Form/Field.php
+++ app/code/community/Lanot/Core/Block/Adminhtml/System/Config/Form/Field.php
@@ -1,21 +1,12 @@
 <?php
 /**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
+ * Private Entrepreneur Anatolii Lehkyi (aka Lanot)
  *
  * @category    Lanot
  * @package     Lanot_Core
- * @copyright   Copyright (c) 2012 Lanot
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright   Copyright (c) 2010 Anatolii Lehkyi
+ * @license     http://opensource.org/licenses/osl-3.0.php
+ * @link        http://www.lanot.biz/
  */
 
 class Lanot_Core_Block_Adminhtml_System_Config_Form_Field
